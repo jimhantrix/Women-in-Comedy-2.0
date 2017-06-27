b388709b80595865370884afc9b46c4324fb3967
sagar-miglani <smiglani1994@gmail.com>
2017-06-26 20:56:56 -0700

    created user, events, post api

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,10 +11,33 @@ class Api::UsersController < ApplicationController
 	end
 
 	def update
+		@user = User.find(params[:id])
 		
+		@user.update(
+		first_name: params[:first_name],
+		last_name: params[:last_name],
+		birthday: params[:birthday],
+		about: params[:about],
+		avatar: params[:avatar],
+		video_link: params[:video_link],
+		location: params[:location],
+		website: params[:website],
+		training: params[:training],
+		experience: params[:experience]
+		)
+
+		render 'show.json.jbuilder'
 	end
 
 	def destroy
+		user = User.find(params[:id])
+		user.destroy
+	
+		# @users = User.all
+		# render 'index.json.jbuilder'
+
+		UsersController.index
+		#needs testing
 	end
 
 end
