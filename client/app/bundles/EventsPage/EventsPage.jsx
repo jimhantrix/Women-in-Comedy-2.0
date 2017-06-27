de4e9812216e551a464bd0bf5af0a963c23bdaed
noelle <saltanoelle@gmail.com>
2017-06-27 12:22:34 -0700

    create component and added styles

--- client/app/bundles/EventsPage/EventsPage.jsx
+++ client/app/bundles/EventsPage/EventsPage.jsx
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import EventsPageComponent from './components/EventsPageComponent';
+import AddEvent from './components/AddEvent';
 export default class EventsPage extends Component {
 	render () {
 		return (
 			<div>
-				<h1>Events Page</h1>
-				< EventsPageComponent />
+			
+        <AddEvent/>
 			</div>
 		);
 
---
+++ client/app/bundles/EventsPage/components/AddEvent.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+export default (props) => {
+  return (
+  
+      <div className="add-event col-md-4">
+       
+
+
+       <h3>Upcoming Events</h3>
+       <p>Past Events</p> 
+       <button className="button">+NEW EVENT</button>
+      
+      
+      
+    
+  
+    </div>
+  
+  );
+
+};
+
+
+
+
+
+
